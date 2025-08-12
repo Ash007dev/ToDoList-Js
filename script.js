14ff31f2c2dcd8e8c884cc905f7ee1501d89b571
Ash <ashish007tup@gmail.com>
2025-08-12 16:32:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const taskArray = []
+let taskArray = []
 let container = document.querySelector('.toDoListContainer');
+
+function saveToStorage(){
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+taskArray = JSON.parse(localStorage.getItem('todoList')) || [{
+  name: 'Make Dinner',
+  dueDate: '2025-08-12'
+}, {
+  name: 'Wash Dishes',
+  dueDate: '2025-08-12'
+}];
+
+renderToDoList();
+
+
 function renderToDoList(){
     let todoListHTML = "";
 
@@ -9,22 +25,25 @@ function renderToDoList(){
         //const dueDate = toDoObject.dueDate;
         const { name, dueDate } = toDoObject;
         const html = `<div class="taskRow">
-                      <div>
-                        ${name}
-                      </div>
-                      <div>
-                        ${dueDate}
-                      </div>
-                      <button class="deleteButton" onclick="taskArray.splice(${i}, 1); renderToDoList();">
-                        Delete
-                      </button>
+                        <div>
+                            ${name}
+                        </div>
+                        <div>
+                            ${dueDate}
+                        </div>
+                        <button class="deleteButton" onclick="taskArray.splice(${i}, 1); renderToDoList(); saveToStorage();">
+                            Delete
+                        </button>
                     </div>`;
         todoListHTML += html;
     }
     container.innerHTML = todoListHTML;
+
+
 }
 
 function addTask(){
+    
     const taskName = document.querySelector('.taskName').value;
     console.log(taskName);
     const dateofTask = document.querySelector('.dateofTask').value;
@@ -38,6 +57,8 @@ function addTask(){
     console.log(taskArray);
 
     renderToDoList();
+
+    saveToStorage();
 
 }
 
